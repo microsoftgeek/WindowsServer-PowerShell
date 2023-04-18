ea974b5ad77d5d3d8dc3ea5953ed45da551f08d7
Cesar G. Duran Solano <55266558+microsoftgeek@users.noreply.github.com>
2023-04-17 20:45:25 -0500

    Update JoinServer2Domain.ps1

--- JoinServer2Domain.ps1
+++ JoinServer2Domain.ps1
@@ -1,5 +1,5 @@
-﻿get-credential corptcf\a.t281937
+﻿get-credential corp\username
 
-Add-Computer -ComputerName MNP-RATA-APP01 -DomainName "CORPTCF" -OUPath "OU=virtual servers,OU=servers,DC=corp,DC=tcf,DC=biz" -Restart
+Add-Computer -ComputerName MNP-RATA-APP01 -DomainName "CORP" -OUPath "OU=virtual servers,OU=servers,DC=corp,DC=company,DC=biz" -Restart
 
-Add-Computer -DomainName "CORPTCF" -OUPath "OU=virtual servers,OU=servers,DC=corp,DC=tcf,DC=biz" -Restart
+Add-Computer -DomainName "CORP" -OUPath "OU=virtual servers,OU=servers,DC=corp,DC=company,DC=biz" -Restart
